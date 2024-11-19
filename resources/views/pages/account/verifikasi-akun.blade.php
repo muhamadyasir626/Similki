5dd6e828dc631a9cd78aa768eae81132b218f22c
muhamadyasir626 <muhamadyasir626@gmail.com>
2024-11-19 15:33:20 +0700

    modifikasi button status permission dan direct page jika iddle

--- resources/views/pages/account/verifikasi-akun.blade.php
+++ resources/views/pages/account/verifikasi-akun.blade.php
@@ -2,6 +2,9 @@
 
 @push('plugin-styles')
   <link href="{{ asset('assets/plugins/datatables-net-bs5/dataTables.bootstrap5.css') }}" rel="stylesheet" />
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
+
+
 @endpush
 
 @section('content')
@@ -22,17 +25,19 @@
             <thead>
                 <tr>
                     <th>Nama Lengkap</th>
-                    <th>Role</th>
-                    <th>Permission</th>
+                    <th style="text-align: center;">Role</th>
+                    <th style="text-align: center;" >Status</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($user as $user)
                     <tr>
                         <td>{{ $user->nama_lengkap }}</td>
-                        <td>{{ $user->role->name }} ({{ $user->role->tag}})</td> <!-- Nama UPT melalui relasi, gunakan null check -->
-                        <td>            
-                          <input type="checkbox" data-id="{{ $user->id }}" {{ $user->status_permission ? 'checked' : '' }} class="toggle-class" data-toggle="toggle" data-style="ios">
+                        <td style="text-align: center;">{{ $user->role->name }} ({{ $user->role->tag}})</td> <!-- Nama UPT melalui relasi, gunakan null check -->
+                        <td style="text-align: center;">            
+                          <input type="checkbox" data-id="{{ $user->id }}" {{ $user->status_permission ? 'checked' : '' }} 
+                          class="toggle-class" data-toggle="toggle" data-on="Diizinkan" data-off="Tidak Diizinkan" 
+                          data-onstyle="success" data-offstyle="danger">
                         </td>
                 
                     </tr>
@@ -57,21 +62,31 @@
 <script>
 $(document).ready(function() {
     $('.toggle-class').change(function() {
-        var status = $(this).prop('checked') == true ? 1 : 0;
+        var status = $(this).prop('checked') ? 1 : 0;
         var userId = $(this).data('id');
-        console.log(userId+"->"+ status);
+        console.log(userId + " -> " + status);
 
         $.ajax({
             type: "POST",
             dataType: "json",
             url: `/updated-permission/id=${userId}`,
-            data: {'status': status, '_token': '{{ csrf_token() }}'},
-            success: function(data){
-              // console.log(data.message);
+            data: {
+                'status': status,
+                '_token': '{{ csrf_token() }}'
+            },
+            success: function(data) {
+                console.log('Permission updated successfully');
+            },
+            error: function(xhr, status, error) {
+                console.error('An error occurred:', error);
             }
         });
     });
 });
+
 </script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap-toggle/js/bootstrap-toggle.min.js"></script>
 
 @endpush
