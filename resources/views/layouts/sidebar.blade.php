e2388c311b9de219192f40fb95087bc897c23544
sagistarz <novialestarin@gmail.com>
2024-12-17 11:45:09 +0700

    add ubah warna untuk active link menu sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,3 +1,7 @@
+@push('plugin-styles')
+<link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
+@endpush
+
 <nav class="sidebar">
   <div class="sidebar-header">
     <a href="#" class="sidebar-brand" id="{{ strtolower($user->role->tag) }}">
@@ -13,52 +17,61 @@
   <div class="sidebar-body">
     <ul class="nav">
       <li class="nav-item nav-category">Main</li>
-      <li class="nav-item {{ active_class(['/']) }}">
+      {{-- <li class="nav-item {{ active_class(['/']) }}">
+        <a href="{{ url('/dashboard') }}" class="nav-link">
+          <i class="link-icon" data-feather="home"></i>
+          <span class="link-title">Dashboard</span>
+        </a>
+      </li> --}}
+
+      <li class="nav-item {{ active_class(['dashboard']) }}">
         <a href="{{ url('/dashboard') }}" class="nav-link">
           <i class="link-icon" data-feather="home"></i>
           <span class="link-title">Dashboard</span>
         </a>
       </li>
+      
       <li class="nav-item nav-category">LEMBAGA KONSERVASI</li>
-      <li class="nav-item {{ active_class(['/']) }}">
+      
+      <li class="nav-item {{ active_class(['lembaga-konservasi']) }}">
         <a href="{{ route('lembaga-konservasi.index') }}" class="nav-link">
-          <i class="link-icon" data-feather="list"></i>
-          <span class="link-title">Daftar Isi LK</span>
+          <i class="link-icon" data-feather="monitor"></i>
+          <span class="link-title">Daftar isi LK</span>
         </a>
       </li>
-      <li class="nav-item {{ active_class(['/']) }}">
+      <li class="nav-item {{ active_class(['monitoring']) }}">
         <a href="{{ route('monitoring-lk') }}" class="nav-link">
           <i class="link-icon" data-feather="monitor"></i>
           <span class="link-title">Monitoring LK</span>
         </a>
       </li>
-      <li class="nav-item {{ active_class(['/']) }}">
-        <a href="" class="nav-link">
+      <li class="nav-item {{ active_class(['lembaga-konservasi/create']) }}">
+        <a href="{{ url('/lembaga-konservasi/create') }}" class="nav-link">
           <i class="link-icon" data-feather="plus-circle"></i>
           <span class="link-title">Pendataan LK </span>
         </a>
       </li>
-      <li class="nav-item {{ active_class(['/']) }}">
-        <a href="" class="nav-link">
+      <li class="nav-item {{ active_class(['lembaga-konservasi/import']) }}">
+        <a href={{ route('import-lk') }} class="nav-link">
           <i class="link-icon" data-feather="upload"></i>
           <span class="link-title">Import File</span>
         </a>
       </li>
-      <li class="nav-item {{ active_class(['/']) }}">
-        <a href="" class="nav-link">
+      <li class="nav-item {{ active_class(['lembaga-konservasi/export']) }}">
+        <a href="{{ route('export-lk') }}" class="nav-link">
           <i class="link-icon" data-feather="download"></i>
           <span class="link-title">Export File </span>
         </a>
       </li>
 
       <li class="nav-item nav-category">SATWA</li>
-      <li class="nav-item {{ active_class(['/']) }}">
+      <li class="nav-item {{ active_class(['satwa']) }}">
         <a href="{{ route('satwa.index') }}" class="nav-link">
           <i class="link-icon" data-feather="list"></i>
           <span class="link-title">Daftar Isi Satwa </span>
         </a>
       </li>
-      <li class="nav-item {{ active_class(['/']) }}">
+      <li class="nav-item {{ active_class(['pendataan-satwa']) }}">
         <a href="{{ route('form-satwa') }}" class="nav-link">
           <i class="link-icon" data-feather="plus-circle"></i>
           <span class="link-title">Pendataan Satwa </span>
